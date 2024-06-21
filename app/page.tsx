a142fc9fa7ba6d7693404d280649e248d7f05699
Slavinto <slava3669@gmail.com>
2024-06-21 16:03:25 +0300

    Next-themes theme toggle function added

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { BackgroundGradient } from "@/components/ui/background-gradient";
 
 export default function Home() {
     return (
-        <main className='px-4 py-8 w-full h-screen flex flex-col border border-red-50'>
+        <main className='px-4 py-8 w-full h-screen flex flex-col bg-blue-600'>
             <Hero />
         </main>
     );
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import {
     heroMainHeading,
@@ -7,11 +8,13 @@ import {
     heroCTAButtonText,
 } from "@/data";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { useTheme } from "next-themes";
 
 const Hero = () => {
+    const { theme, setTheme } = useTheme();
     const words = "Bringing you fast and reliable solutions for the web";
     return (
-        <section className='bg-background w-full h-32 flex flex-col border border-red-500'>
+        <section className='bg-red-500 rounded-2xl border-2 w-full h-32 flex flex-col items-center'>
             <h3 className='uppercase font-normal'></h3>
             <h1 className='text-white text-center font-bold text-6xl '>
                 <TextGenerateEffect
@@ -19,6 +22,12 @@ const Hero = () => {
                     className='!text-white-100 font-bold text-5xl'
                 />
             </h1>
+            <button
+                className='border-2 rounded-xl p-2 w-32'
+                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            >
+                Toggle Theme
+            </button>
         </section>
     );
 };
