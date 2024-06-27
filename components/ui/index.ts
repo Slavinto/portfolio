537721217c34b431005a34209c75283f63169e8b
Slavinto <slava3669@gmail.com>
2024-06-27 08:47:36 +0300

    Started working on grid - created custom grid component - no changes yet

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,3 +2,4 @@ export { BackgroundGradient } from "./background-gradient";
 export { ButtonsCard } from "./tailwindcss-buttons";
 export { TextGenerateEffect } from "./text-generate-effect";
 export { default as DotBackground } from "./dot-background";
+export { BentoGrid, BentoGridItem } from "./bento-grid";
--- data/index.ts
+++ data/index.ts
@@ -6,3 +6,37 @@ export const heroIntroductionText =
     "Hi, I'm Slava! A Next.js developer working from Russia.";
 export const heroCTAButtonText = "See my work";
 // ==========================hero=section======================
+// ==========================grid=section======================
+export const aboutMeGridItems = [
+    {
+        id: 1,
+        text: "I prioritize analytical thinking, which helps to find the best solution for the problem",
+        image: "/notebook.png",
+    },
+    {
+        id: 2,
+        text: "I pay attention to detail - this is important for quality code",
+        image: "/notebook.png",
+    },
+    {
+        id: 3,
+        text: "I prioritize client collaboration, fostering open communication",
+        image: "/notebook.png",
+    },
+    {
+        id: 4,
+        text: "I prioritize client collaboration, fostering open communication",
+        image: "/notebook.png",
+    },
+    {
+        id: 5,
+        text: "I prioritize client collaboration, fostering open communication",
+        image: "/notebook.png",
+    },
+    {
+        id: 6,
+        text: "I prioritize client collaboration, fostering open communication",
+        image: "/notebook.png",
+    },
+];
+// ==========================grid=section======================
