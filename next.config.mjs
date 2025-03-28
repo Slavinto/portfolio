18f6abcde29edf85042401fd0b89cf4d12d450f2
Slavinto <slava3669@gmail.com>
2025-03-28 11:01:48 +0300

    remove sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: "export",
-    // distDir: "dist",
     images: {
-        // unoptimized: true,
+        unoptimized: true,
         remotePatterns: [
             { protocol: "https", hostname: "images.unsplash.com" },
         ],
     },
-    // basePath: "/portfolio",
-    // assetPrefix: "/portfolio/",
 };
 
 export default nextConfig;
